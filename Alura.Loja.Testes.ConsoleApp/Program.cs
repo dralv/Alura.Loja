c27fb9f22ea431d9e144cd345d5e2316e8d7dc62
Alvaro <dralvphd2@gmail.com>
2022-11-14 10:50:23 -0300

    Excluindo com Entity

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -14,6 +14,22 @@ namespace Alura.Loja.Testes.ConsoleApp
             //GravandoUsandoEntity();
 
             RecuperarProdutos();
+            ExcluirProdutos();
+            RecuperarProdutos();
+        }
+
+        private static void ExcluirProdutos()
+        {
+           using(var repo = new LojaContext()) 
+           {
+                IList<Produto> produtos = repo.Produtos.ToList();
+                foreach (var item in produtos)
+                {
+                    repo.Produtos.Remove(item);
+                    
+                }
+                repo.SaveChanges(); 
+           }
         }
 
         private static void RecuperarProdutos()
@@ -21,6 +37,7 @@ namespace Alura.Loja.Testes.ConsoleApp
             using(var repo = new LojaContext())
             {
                 IList<Produto> produtos = repo.Produtos.ToList();
+                Console.WriteLine($"Foram encontrados {produtos.Count} produto(s).");
 
                 foreach (var item in produtos)
                 {
